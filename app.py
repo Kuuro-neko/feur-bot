621dc0867d47d30b46d66a557429011d6b9f1cb5
Gilles Gonzalez Oropeza <95430715+Kuuro-neko@users.noreply.github.com>
2023-02-04 11:39:07 +0000

    memegen command

--- app.py
+++ app.py
@@ -1,16 +1,49 @@
 # bot.py
 import os
-
 import discord
+from discord import app_commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-print(type(TOKEN))
 client = discord.Client(intents=discord.Intents(message_content=True, messages=True))
+tree = app_commands.CommandTree(client)
+
+def get_file_extension(filename):
+    return filename.split(".")[-1]
+
+def replace_special_chars_memegen(string):
+    return string.replace("-", "--").replace("_", "__").replace(" ", "_").replace("?", "~q").replace("&", "~a").replace("%", "~p").replace("#", "~h").replace("/", "~s").replace("\"", "''").replace("<", "~l").replace(">", "~g")
+
+@tree.command(name = "memegen")
+async def memegen(interaction, image: str="", top: str="", bottom: str=""):
+    """Génère un meme avec le texte en haut et en bas
+
+    Parameters
+    -----------
+    top: str
+        Texte à afficher en haut de l'image
+    
+    bottom: str
+        Texte à afficher en bas de l'image
+
+    image: str
+        Lien vers l'image à utiliser
+    """
+    if image == None:
+        await interaction.response.send_message("Il faut mettre une image")
+        return
+    ext = get_file_extension(image)
+    if ext not in ["jpg", "jpeg", "png", "gif"]:
+        await interaction.response.send_message("L'image doit être un jpg, un png ou un gif")
+        return
+    top = replace_special_chars_memegen(top)
+    bottom = replace_special_chars_memegen(bottom)
+    await interaction.response.send_message(f"https://api.memegen.link/images/custom/{top}/{bottom}.{ext}?background={image}")
 
 @client.event
 async def on_ready():
+    await tree.sync()
     print(f'{client.user} has connected to Discord!')
 
 @client.event
@@ -22,5 +55,6 @@ async def on_message(message):
             f.write(new_line + "\n")
     if  "quoi" in message.content.lower():
         await message.channel.send("Feur", reference=message)
+
 
 client.run(TOKEN)
